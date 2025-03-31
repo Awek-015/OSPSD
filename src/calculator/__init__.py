461e10f4f738974603b1b5373589fcbceb04d737
Haoran Zhou <hz3575@nyu.edu>
2025-03-31 03:07:06 -0400

    Initial interface design commit

--- src/calculator/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Calculator package for basic arithmetic operations."""
-
-from .api import add, multiply, subtract
-
-__all__ = ["add", "subtract", "multiply"]
--- src/notifier/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Notifier package for notification operations."""
-
-from .api import create_notifier, notify
-
-__all__ = ["create_notifier", "notify"]
